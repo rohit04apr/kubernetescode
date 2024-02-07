83376566f2e4ce4bec0afc8758aa21779a9165bd
Rohit Tiwari <rohit.tiwari@farfetch.com>
2024-02-07 21:59:06 +0000

    [FEAT] flask changes

--- app.py
+++ app.py
@@ -2,5 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!!'
+def hello_geek():
+    return '<h1>Hello from Flask & Docker</h2>'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
